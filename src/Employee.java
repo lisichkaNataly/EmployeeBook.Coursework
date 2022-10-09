d77ffb8e545703c09121558da5a9614e4b5f5cd2
Izmaylova <Natalyatf@yandex.ru>
2022-10-09 22:10:30 +0500

    Базовая сложность

---
+++ src/Employee.java
@@ -0,0 +1,64 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Employee {
+
+    private String FIO;
+    private int department;
+    private int salary;
+    private static int Counter;
+    private int id;
+
+
+    public Employee (String fio, int department, int salary) {
+        this.FIO = fio;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++Counter;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getFIO() {
+        return this.FIO;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public void setFIO(String FIO) {
+        this.FIO = FIO;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return " ID: " + id + " ФИО: " + FIO + " Отдел: " + department + " Зарплата: " + salary;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import java.util.stream.IntStream;
+
 public class Main {
+
+    private static final Employee[] employees = new Employee[10];
+
     public static void main(String[] args) {
 
+        employees[0] = new Employee("Иванова Дарья Алексеевна", 1, 40000);
+        employees[1] = new Employee("Измайлов Максим Андреевич", 2, 175000);
+        employees[2] = new Employee("Кырнац Людмила Вячеславовна", 3, 79000);
+        employees[3] = new Employee("Анфалова Елена Романовна", 2, 63000);
+        employees[4] = new Employee("Рагозин Сергей Анатольевич", 1, 59000);
+        employees[5] = new Employee("Овчинников Андрей Федорович", 3, 46000);
+        employees[6] = new Employee("Дрягин Павел Сергеевич", 4, 220000);
+        employees[7] = new Employee("Никитин Владислав Александрович", 4, 193000);
+        employees[8] = new Employee("Шатрова Ирина Владимировна", 5, 105000);
+        employees[9] = new Employee("Исаев Александр Николаевич", 5, 128000);
+
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
+        System.out.println("Сумма всех зарплат: " + calculateSumsalary() + " рублей");
+
+        Employee employeeWithMaxSalary = employeeWitMaxSalary();
+        if(employeeWithMaxSalary == null){
+            System.out.println("Нет сотрудника с высокой зарплатой");
+        } else {
+            System.out.println("Сотрудник с максимальной зарплатой: " + employeeWitMaxSalary());
+        }
+
+        System.out.println("Сотрудник с минимальной зарплатой: " + employeeWithMinSalary());
+    }
+
+    public static int calculateSumsalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                sum += employee.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static Employee employeeWitMaxSalary() {
+        int max = Integer.MIN_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() > max) {
+                max = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
+    }
+
+    public static Employee employeeWithMinSalary() {
+        int min = Integer.MAX_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() < min) {
+                min = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
     }
 }
